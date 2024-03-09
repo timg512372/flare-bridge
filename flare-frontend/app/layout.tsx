548bf82ea3f3c1340ef9627c6cc3a9df27f59feb
Ezra Tramble <dr.etramble@icloud.com>
2024-03-09 15:17:32 +0000

    Added wagmi/wallet connect and shad

--- flare-frontend/app/layout.tsx
+++ flare-frontend/app/layout.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.css"
-import { Metadata } from "next"
+import { Metadata, Viewport } from "next"
 
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
@@ -7,6 +7,13 @@ import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+import { headers } from 'next/headers'
+
+import { cookieToInitialState } from 'wagmi'
+
+import { config } from '@/config'
+import { ContextProvider } from '@/context'
+
 
 export const metadata: Metadata = {
   title: {
@@ -14,22 +21,26 @@ export const metadata: Metadata = {
     template: `%s - ${siteConfig.name}`,
   },
   description: siteConfig.description,
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
 }
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+}
+
 
 interface RootLayoutProps {
   children: React.ReactNode
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
+  const initialState = cookieToInitialState(config, headers().get('cookie'))
   return (
     <>
       <html lang="en" suppressHydrationWarning>
@@ -40,13 +51,16 @@ export default function RootLayout({ children }: RootLayoutProps) {
             fontSans.variable
           )}
         >
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <div className="relative flex min-h-screen flex-col">
-              <SiteHeader />
-              <div className="flex-1">{children}</div>
-            </div>
-            <TailwindIndicator />
-          </ThemeProvider>
+            <ContextProvider initialState={initialState}>
+              <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+                <div className="relative flex flex-col min-h-screen">
+                  <SiteHeader />
+                  <div className="flex-1">{children}</div>
+                </div>
+                <TailwindIndicator />
+              </ThemeProvider>
+            </ContextProvider>
+        
         </body>
       </html>
     </>
--- flare-frontend/components/site-header.tsx
+++ flare-frontend/components/site-header.tsx
@@ -1,52 +1,67 @@
+"use client"
 import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
+import { useWeb3Modal } from "@web3modal/wagmi/react"
+import { useAccount, useDisconnect } from "wagmi"
+import { Label } from "@radix-ui/react-label"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
 export function SiteHeader() {
+  const { open } = useWeb3Modal();
+  const { address, isConnecting, isDisconnected } = useAccount();
+  const { disconnect } = useDisconnect()
+
+
   return (
     <header className="bg-background sticky top-0 z-40 w-full border-b">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-1">
-            <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "icon",
-                  variant: "ghost",
-                })}
-              >
-                <Icons.gitHub className="h-5 w-5" />
-                <span className="sr-only">GitHub</span>
-              </div>
-            </Link>
-            <Link
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "icon",
-                  variant: "ghost",
-                })}
-              >
-                <Icons.twitter className="h-5 w-5 fill-current" />
-                <span className="sr-only">Twitter</span>
-              </div>
-            </Link>
+          <nav className="flex items-center space-x-2">
+            {address ? (
+              <><AlertDialog>
+                <AlertDialogTrigger>
+                  <Button>
+                    Connected
+                    <span className="ml-2 h-2 w-2 bg-green-500 rounded-full inline-block"></span>
+                  </Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>Disconnect?</AlertDialogTitle>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={() => disconnect()}>Disconnect</AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+              </>
+            ) : (
+              <Button onClick={() => open()}>Connect Wallet</Button>
+            )}
             <ThemeToggle />
           </nav>
         </div>
       </div>
     </header>
   )
+}
+export function ConnectButton() {
+  return <w3m-button />
 }
